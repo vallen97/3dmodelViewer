c1731550b828ee9a370876bb3b9f9721a850322a
vallen97 <vaughnallen97@gmail.com>
2019-05-04 19:05:46 -0600

    Made Changes from Email
    Made changed from email dated: May 4

--- 3DModelViewer/Assets/Scripts/ButtonController.cs
+++ 3DModelViewer/Assets/Scripts/ButtonController.cs
@@ -24,6 +24,8 @@ public class ButtonController : MonoBehaviour
     private Button btnCredits;
 
     // text
+    private Text txtModelTitle;
+    private Text txtModelLocat;
     private Text txtModelDesc;   // model description text
     private Text txtPageNum;     // page number
 
@@ -41,13 +43,13 @@ public class ButtonController : MonoBehaviour
     // model names model gameobject and model
     private string[] modelNames = { "model/21370.11", "model/21393.11", "model/Axe Head", "model/LateGlazeBowl Touchup", "model/pot 2 blender", "model/Pot1Blender_", "model/Pot2Blender_" };
     // credits
-    private static string credits = "Credits: \nVaughn Allen\n";
+    private static string credits = "Frank Naranjo,\nVaughn Allen,\nMireya Rodriguez,\nBecca Sharp";
 
     // list
     private List<GameObject> models;    // list of allmodels
     private List<GameObject> buttons;   // create buttons for model buttons
     private List<Button> modelButtons;  // list of all model buttons
-    private List<string[]> modelDesc;   // list of all model descriptions
+    private List<List<string[]>> modelDesc;   // list of all model descriptions
     private List<Sprite> buttonImages;  // sprites for button images
 
     // gameObject
@@ -74,6 +76,9 @@ public class ButtonController : MonoBehaviour
     [SerializeField]
     private Sprite btn8;
 
+    // variable to check if button is pressed and model description  wasn't open
+    private bool isCredits = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -84,7 +89,7 @@ public class ButtonController : MonoBehaviour
         // instanciate list
         models = new List<GameObject>();
         modelButtons = new List<Button>();
-        modelDesc = new List<string[]>();
+        modelDesc = new List<List<string[]>>();
         buttons = new List<GameObject>();
         buttonImages = new List<Sprite>();
 
@@ -154,6 +159,8 @@ public class ButtonController : MonoBehaviour
         btnCredits = GameObject.Find("btnCredits").GetComponent<Button>();
 
         // find text box
+        txtModelTitle = GameObject.Find("txtModelTitle").GetComponent<Text>();
+        txtModelLocat = GameObject.Find("txtModelLocat").GetComponent<Text>();
         txtModelDesc = GameObject.Find("txtModelDesc").GetComponent<Text>();
         txtPageNum = GameObject.Find("txtPageNum").GetComponent<Text>();
 
@@ -171,9 +178,6 @@ public class ButtonController : MonoBehaviour
         modelButtons[6].onClick.AddListener(() => ModelChange(6));
         //modelButtons[7].onClick.AddListener(() => ModelChange(7));
 
-        // hide button wo aren't using
-        modelButtons[7].gameObject.SetActive(false);
-
         // info button
         btnModelDesc.onClick.AddListener(() => DisplayModelDesc());
 
@@ -187,6 +191,107 @@ public class ButtonController : MonoBehaviour
         // Set model descriptions into list
         // NOTE: adding a new string ie "new", "line", line will be on a the next page
         // more about formatting text using HTML: https://docs.unity3d.com/Manual/StyledText.html
+        modelDesc.Add(new List<string[]> {
+            new string[]
+            {
+                "<b>Small Jar</b>"
+            },
+            new string[]
+            {
+                "<i>Jemez Black-on-white</i>\n<i>AD 1600-1700</i>"
+            },
+            new string[]
+            {
+                "This jar was likely used as a canteen. It was excavated from the nearby ancient Jemez village of Unshagi. Two sparrows can be seen on each side of the vessel. There is also a hole drilled near the mouth which was probably used for a carrying cord."
+            }
+        });
+        modelDesc.Add(new List<string[]> {
+            new string[]
+            {
+                "<b>Stepped Bowl</b>"
+            },
+            new string[]
+            {
+                "<i>Jemez Black-on-white</i>\n<i>AD 1300-1750</i>"
+            },
+            new string[]
+            {
+                "This partially restored bowl was excavated from a nearby ancient Jemez village. Notice the repeating stairstep motifs all over this vessel. It was common in the early 1900s for archaeologists to “repair” or “recreate” missing portions of pottery.Can you identify where the early",
+                "archaeologists added to this vessel?"
+            }
+        });
+        modelDesc.Add(new List<string[]> {
+            new string[]
+            {
+                "<b>Metal Axe</b>"
+            },
+            new string[]
+            {
+                "<i>AD 1700-1800</i>"
+            },
+            new string[]
+            {
+                "Before the arrival of the Spanish, most pueblo tools were made of wood, ceramic, stone, and bone. The introduction of metal tools changed the way people in the village of Giusewa lived. It takes half of the time to chop down a tree with a metal axe compared to a stone axe.7"
+            }
+        });
+        modelDesc.Add(new List<string[]> {
+            new string[]
+            {
+                "<b>Trade Bowl</b>"
+            },
+            new string[]
+            {
+                "<i>Rio Grande Glazeware</i>\n<i>AD 1515-1750</i>"
+            },
+            new string[]
+            {
+                "While most of the pottery found at the ancient village of Giusewa is Jemez Black-on-white, recent excavations have revealed a large amount of Rio Grande Glazeware. These colorful pieces of pottery were made in the ancestral villages of Zia and Tamaya, and traded into the Jemez Valley."
+            }
+        });
+        modelDesc.Add(new List<string[]> {
+            new string[]
+            {
+                "<b>Shipping Jar</b>"
+            },
+            new string[]
+            {
+                "<i>Andalusian Utilitywear</i>\n<i>AD 1580-1850</i>"
+            },
+            new string[]
+            {
+                "These large egg-shaped jars were produced in Spain, filled with goods, then transported to the North American Colonies. Jars of this shape typically held wine, olive oil, olives. Once the contents of the jars were depleted, they were reused as storage jars for a variety of products made in the New World."
+            }
+        });
+        modelDesc.Add(new List<string[]> {
+            new string[]
+            {
+                "<b>Cooking Pot</b>"
+            },
+            new string[]
+            {
+                "<i>Jemez Utilitywear</i>\n<i>AD 1300-1850</i>"
+            },
+            new string[]
+            {
+                "More than half of the pottery excavated from Giusewa Pueblo were fragments of grey and black Utilitywear. These vessels were used for cooking and storing food.It was an effective form of pottery, so it changed very little over the course of several hundred years."
+            }
+        });
+        modelDesc.Add(new List<string[]> {
+            new string[]
+            {
+                "<b>Miniature Canteen</b>"
+            },
+            new string[]
+            {
+                "<i>Jemez Black-on-white</i>\n<i>AD 1600-1700</i>"
+            },
+            new string[]
+            {
+                "This miniature canteen was found on the floor of a room here in the village of Giusewa. It was excavated in 2019 and appears to still have materials inside. These will be tested to reveal the canteen’s contents. Notice the detailed quartered-designs on the face of the vessel."
+            }
+        });
+
+        /*
         modelDesc.Add(new string[]{
             "<b>Small Jar</b>\n<i>Jemez Black-on-white</i>\nAD 1300-1750\n\nThis jar was likely used as a canteen. It was excavated from the nearby ancient Jemez village of Unshagi. Two sparrows can be seen on each side of the vessel. There is also a hole drilled near the mouth which was probably used for a carrying cord."
         });
@@ -214,9 +319,13 @@ public class ButtonController : MonoBehaviour
 
         modelDesc.Add(new string[]{
             "<b>Miniature Canteen</b>\n<i>Jemez Black-on-white</i>\nAD 1600-1700\n\nThis miniature canteen was found on the floor of a room here in the village of Giusewa. It was excavated in 2019 and appears to still have materials inside. These will be tested to reveal the canteen’s contents. Notice the detailed quartered-designs on the face of the vessel."
-        });
+        });*/
+
+
 
         // hide UI elements
+        txtModelTitle.gameObject.SetActive(false);
+        txtModelLocat.gameObject.SetActive(false);
         txtModelDesc.gameObject.SetActive(false);
         imgTxtBackground.gameObject.SetActive(false);
         btnTextF.gameObject.SetActive(false);
@@ -267,10 +376,20 @@ public class ButtonController : MonoBehaviour
         }
         else if (txtModelDesc.gameObject.activeSelf == true)    // if text is showing
         {
-            if (txtModelDesc.text == credits)   // if text showing is credits
+            if (txtModelLocat.text == credits)   // if text showing is credits
             {
-                txtModelDesc.text = modelDesc[activeModel][infoPos];        // change back to model description
-                btnModelDesc.GetComponent<Image>().sprite = sprHideDesc;    // change 
+                if (isCredits == true)
+                {
+                    StartCoroutine("animateBtnTxtHide"); // hide text
+                }
+                else
+                {
+                    txtModelTitle.text = modelDesc[activeModel][0][0];
+                    txtModelLocat.text = modelDesc[activeModel][1][0];
+                    txtModelDesc.text = modelDesc[activeModel][2][infoPos];        // change back to model description
+                    hideButtonArrows(modelDesc[activeModel][2].Length);
+                    btnModelDesc.GetComponent<Image>().sprite = sprHideDesc;    // change button to hide sprite
+                }
             }
             else  // if text is not credits
             {
@@ -279,24 +398,50 @@ public class ButtonController : MonoBehaviour
         }
 
         // which button was pressed and asign model text
-        txtModelDesc.text = modelDesc[activeModel][infoPos];
+        txtModelTitle.text = modelDesc[activeModel][0][0];
+        txtModelLocat.text = modelDesc[activeModel][1][0];
+        txtModelDesc.text = modelDesc[activeModel][2][infoPos];
+        // hide or unhide button arrows
+        hideButtonArrows(modelDesc[activeModel][2].Length);
     }
 
     private void DisplayCredits()
     {
         if (txtModelDesc.gameObject.activeSelf == false)        // if text is not showing
         {
-            txtModelDesc.text = credits;            // change text to credits 
+            isCredits = true;                       // model description is not open
+            txtModelTitle.text = "Credits:";
+            txtModelLocat.text = credits;
+            txtModelDesc.text = " ";            // change text to credits 
+            txtPageNum.text = "1/1";                // change text to 1/1
             StartCoroutine("animateBtnTxtShow");    // show text
+            hideButtonArrows(1);                    // hide page button arrows
         }
         else if (txtModelDesc.gameObject.activeSelf == true)    // if text is showing
         {
-            if (txtModelDesc.text != credits)   // if text is not credits
+            if (txtModelLocat.text != credits)   // if text is not credits
             {
-                txtModelDesc.text = credits; // change text to credits
+                isCredits = false;
+                txtModelTitle.text = "Credits:";
+                txtModelLocat.text = credits;
+                txtModelDesc.text = " ";            // change text to credits
+                txtPageNum.text = "1/1";                // change text to 1/1
                 btnModelDesc.GetComponent<Image>().sprite = sprShowDesc; // change sprite to info
+                hideButtonArrows(1);                    // hide page button arrows
             }
-            else  // if text is credits
+            else if (isCredits == false)
+            {
+                btnModelDesc.GetComponent<Image>().sprite = sprHideDesc;    // change button to hide sprite
+                // which button was pressed and asign model text
+                txtModelTitle.text = modelDesc[activeModel][0][0];
+                txtModelLocat.text = modelDesc[activeModel][1][0];
+                txtModelDesc.text = modelDesc[activeModel][2][infoPos];
+                txtPageNum.text = "" + (infoPos + 1) + "/" + modelDesc[activeModel][2].Length;
+                // if current models description is 1 or one page don't show the page buttons
+                // hide page button arrows
+                hideButtonArrows(modelDesc[activeModel][2].Length);
+            }
+            else if (isCredits == true) // if text is credits
             {
                 StartCoroutine("animateBtnTxtHide");    // hide text
             }
@@ -318,9 +463,9 @@ public class ButtonController : MonoBehaviour
             // increment position
             infoPos++;
             // make sure within array
-            if (infoPos >= modelDesc[activeModel].Length)
+            if (infoPos >= modelDesc[activeModel][2].Length)
             {
-                infoPos = modelDesc[activeModel].Length - 1;
+                infoPos = modelDesc[activeModel][2].Length - 1;
             }
         }
         else if (dir == 0)
@@ -332,7 +477,9 @@ public class ButtonController : MonoBehaviour
             }
         }
         // set info to text
-        txtModelDesc.text = modelDesc[activeModel][infoPos];
+        txtModelTitle.text = modelDesc[activeModel][0][0];
+        txtModelLocat.text = modelDesc[activeModel][1][0];
+        txtModelDesc.text = modelDesc[activeModel][2][infoPos];
         // checkk info pos
         checkInfoPos();
     }
@@ -343,7 +490,7 @@ public class ButtonController : MonoBehaviour
         btnTextF.interactable = true;
         btnTextB.interactable = true;
 
-        if (infoPos >= modelDesc[activeModel].Length - 1) // if page is to the most right
+        if (infoPos >= modelDesc[activeModel][2].Length - 1) // if page is to the most right
         {
             btnTextF.interactable = false;  // disable forward button
         }
@@ -353,14 +500,29 @@ public class ButtonController : MonoBehaviour
         }
 
         // if there is only one paGe
-        if (modelDesc[activeModel].Length == 1)
+        if (modelDesc[activeModel][2].Length == 1)
         {
             // both buttons are disable
             btnTextF.interactable = false;
             btnTextB.interactable = false;
         }
         // show current page of total
-        txtPageNum.text = "" + (infoPos + 1) + "/" + modelDesc[activeModel].Length;
+        txtPageNum.text = "" + (infoPos + 1) + "/" + modelDesc[activeModel][2].Length;
+    }
+
+    private void hideButtonArrows(int pageNumber)
+    {
+        // if current models description is 1 or one page don't show the page buttons
+        if (pageNumber == 1)
+        {
+            btnTextF.gameObject.SetActive(false);
+            btnTextB.gameObject.SetActive(false);
+        }
+        else
+        {
+            btnTextF.gameObject.SetActive(true);
+            btnTextB.gameObject.SetActive(true);
+        }
     }
 
 
@@ -373,6 +535,8 @@ public class ButtonController : MonoBehaviour
         this.GetComponent<BoxCollider>().center = new Vector3(-4, 0, 0);
 
         // show description
+        txtModelTitle.gameObject.SetActive(true);
+        txtModelLocat.gameObject.SetActive(true);
         txtModelDesc.gameObject.SetActive(true);
         imgTxtBackground.gameObject.SetActive(true);
         btnTextF.gameObject.SetActive(true);
@@ -384,6 +548,8 @@ public class ButtonController : MonoBehaviour
         {
             // move left every .01 seconds
             btnModelDesc.transform.Translate(Vector3.left * (Time.deltaTime * moveText));
+            txtModelTitle.transform.Translate(Vector3.left * (Time.deltaTime * moveText));
+            txtModelLocat.transform.Translate(Vector3.left * (Time.deltaTime * moveText));
             txtModelDesc.transform.Translate(Vector3.left * (Time.deltaTime * moveText));
             btnTextF.transform.Translate(Vector3.left * (Time.deltaTime * moveText));
             btnTextB.transform.Translate(Vector3.left * (Time.deltaTime * moveText));
@@ -406,6 +572,8 @@ public class ButtonController : MonoBehaviour
         {
             // move right every .01 seconds
             btnModelDesc.transform.Translate(Vector3.right * (Time.deltaTime * moveText));
+            txtModelTitle.transform.Translate(Vector3.right * (Time.deltaTime * moveText));
+            txtModelLocat.transform.Translate(Vector3.right * (Time.deltaTime * moveText));
             txtModelDesc.transform.Translate(Vector3.right * (Time.deltaTime * moveText));
             btnTextF.transform.Translate(Vector3.right * (Time.deltaTime * moveText));
             btnTextB.transform.Translate(Vector3.right * (Time.deltaTime * moveText));
@@ -415,6 +583,8 @@ public class ButtonController : MonoBehaviour
         }
 
         // hide description
+        txtModelTitle.gameObject.SetActive(false);
+        txtModelLocat.gameObject.SetActive(false);
         txtModelDesc.gameObject.SetActive(false);
         imgTxtBackground.gameObject.SetActive(false);
         btnTextF.gameObject.SetActive(false);
